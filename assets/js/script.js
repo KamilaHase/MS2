afb5335f5a50c24d3f0c67bb0b1ba950b14ae600
KamilaHase <k.rabinakova@gmail.com>
2021-05-12 16:13:26 +0000

    Added first functions - timer and redirect page to Game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-alert("Hello world");
+
+/*-------adapted from https://www.youtube.com/watch?v=_a4XCarxwr8&t=333s "How to Create a Countdown Timer with JavaScript"---*/
+const timeCountdown = document.getElementById("timer-font-lg");
+let timeSecond = 30;
+
+displayTime(timeSecond);
+
+const countdown = setInterval (() => {
+    timeSecond--;
+    displayTime(timeSecond);
+    if(timeSecond <= 0 || timeSecond < 1){
+        endTime();
+        clearInterval(countdown);
+        window.location.href = "game.html";
+    }
+    
+},1000)
+
+function displayTime(second) {
+    const sec = Math.floor(second % 60);
+    timeCountdown.innerHTML = `00:${sec<10?'0':''}${sec}`;
+}
+
+function endTime(){
+    timeCountdown.innerHTML = `TIME OUT`;
+}
+
+/*
+function redirectToGame(){
+    var timeOutId = setTimeout(function () {
+        window.location.href = "game.html";
+    }, 30*1000);
+} 
+
+*/

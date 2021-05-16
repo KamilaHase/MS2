23f879c55787bcb3e6c9cc546cec144d0653188f
KamilaHase <k.rabinakova@gmail.com>
2021-05-16 15:34:29 +0000

    Shuffle of cards finally working

--- assets/js/scriptGame.js
+++ assets/js/scriptGame.js
@@ -1,4 +1,81 @@
+/* $( function hide() {
+    $( '.draggable-cards' ).hide();
+  } );
+  */
 
+
+//adapted from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array?noredirect=1&lq=1
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (0 !== currentIndex) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  };
+  
+ var arr = $( '.draggable-cards' );
+ shuffle(arr);
+ $('#draggable-cards').append(arr);
+
+ 
+ 
+
+
+  
+
+  
+  
+
+
+/*
+$( function hide() {
+  $( '.draggable-cards' ).hide();
+} );
+*/
+
+/*
+var arr = $( '.draggable-cards' );
+
+function shuffle(arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+        var j = i + Math.floor(Math.random() * (arr.length - i));
+
+        var temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+};
+
+ */
+
+
+
+
+/*
+//adapted accordingly from https://stackoverflow.com/questions/30984794/display-random-div
+ $(document).ready(function() {
+    var cards = $('.draggable-cards');
+    var randomCard1 = Math.floor(Math.random()*cards.length);
+    var visibleCard1 = $('.draggable-cards')[randomCard1];
+    $(visibleCard1).show();
+  
+    var randomCard2 = Math.floor(Math.random()*cards.length);
+    var visibleCard2 = $('.draggable-cards')[randomCard2];
+    $(visibleCard2).show();
+});
+
+*/
+
+// jquery
 $( function() {
     $( "#postylka" ).draggable();
     $( "#okno" ).draggable();
@@ -6,35 +83,6 @@ $( function() {
     $( "#zachod" ).draggable();
     $( "#vaza" ).draggable();
     $( "#lednice" ).draggable();
-
   } );
 
 
-  window.onload = function() {
-    // find the element that you want to drag.
-    var box = document.getElementById('postylka');
-    
-    /* listen to the touchmove event,
-    every time it fires, grab the location
-    of touch and assign it to box */
-    
-    box.addEventListener('touchmove', function(e) {
-      // grab the location of touch
-      var touchLocation = e.targetTouches[0];
-      
-      // assign box new coordinates based on the touch.
-      box.style.left = touchLocation.pageX + 'px';
-      box.style.top = touchLocation.pageY + 'px';
-    })
-    
-  }
-
-  /*record the position of the touch
-  when released using touchend event.
-  This will be the drop position. */
-  
-  box.addEventListener('touchend', function(e) {
-    // current box position.
-    var x = parseInt(box.style.left);
-    var y = parseInt(box.style.top);
-  })

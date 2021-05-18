bf4089a4066f67eb92efa3f7e1c291171952089f
KamilaHase <k.rabinakova@gmail.com>
2021-05-18 18:08:18 +0000

    Started writing the questionnaire fill in JS, added classes of correct-incorrect.

--- assets/js/scriptGame.js
+++ assets/js/scriptGame.js
@@ -1,9 +1,3 @@
-/* $( function hide() {
-    $( '.draggable-cards' ).hide();
-  } );
-  */
-
-
 //adapted from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array?noredirect=1&lq=1
   function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -40,4 +34,76 @@ $( function() {
   $(".btn-reload-page").click(function(){
     location.reload(true);
 });
+
+
+let postylka;
+let okno;
+let kocka;
+let zachod;
+let vaza;
+let lednice;
+
+function submit() {
+  ansPostylka = input-postylka.value;
+  ansOkno = input-okno.value;
+  ansKocka = input-kocka.value;
+  ansZachod = input-zachod.value;
+  ansVaza = input-vaza.value;
+  ansLednice = input-lednice.value;
+
+  if (ansPostylka == "4") {
+    g = 1;
+    input-postylka.value = postylka;
+    // change class to correct
+  } else {
+    input-postylka.value = postylka;
+    // change class to incorrect
+  }
+
+  if (ansOkno == "2") {
+    h = 1;
+    input-okno.value = okno;
+    // change class to correct
+  } else {
+    input-okno.value = okno;
+    // change class to incorrect
+  }
+
+  if (ansKocka == "1") {
+    i = 1;
+    input-kocka.value = kocka;
+    // change class to correct
+  } else {
+    input-kocka.value = kocka;
+    // change class to incorrect
+  }
+
+  if (ansZachod == "3") {
+    j = 1;
+    input-zachod.value = zachod;
+    // change class to correct
+  } else {
+    input-zachod.value = zachod;
+    // change class to incorrect
+  }
+
+  if (ansVaza == "2") {
+    k = 1;
+    input-vaza.value = vaza;
+    // change class to correct
+  } else {
+    input-vaza.value = vaza;
+    // change class to incorrect
+  }
+
+  if (ansLednice == "2") {
+    l = 1;
+    input-lednice.value = lednice;
+    // change class to correct
+  } else {
+    input-lednice.value = lednice;
+    // change class to incorrect
+  }
+
+}
 

f9da208adcfedb5f9b4a3e347bb1b0097dc7350a
KamilaHase <k.rabinakova@gmail.com>
2021-05-24 11:47:53 +0000

    Added instructions, styled Lg version.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,213 @@
+/*--------------------------SHUFFLE THE CARDS AND PICK TWO RANDOM ONES-------------------------------------------------*/
+//adapted from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array?noredirect=1&lq=1
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-/*-------------------------TIMER AND GO TO GAME PAGE-----------------------------------*/
-/* adapted from https://www.youtube.com/watch?v=_a4XCarxwr8&t=333s "How to Create a Countdown Timer with JavaScript" */
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-const timeCountdown = document.getElementById("timer-font-lg");
-let timeSecond = 40;
+  return array;
+}
 
-displayTime(timeSecond);
+var arr = $( '.draggable-cards' );
+shuffle(arr);
+$('#draggable-cards').append(arr);
+$(arr).slice(2).hide();
+ 
 
-const countdown = setInterval (() => {
-    timeSecond--;
-    displayTime(timeSecond);
-    if(timeSecond <= 0 || timeSecond < 1){
-        endTime();
-        clearInterval(countdown);
-        window.location.href = "game.html";
+
+//--------BUTTON RELOAD---------------
+
+$(".btn-reload-page").click(function(){
+location.reload(true);
+});
+
+
+/*---------------------------------QUIZ-------------------*/
+// ----VERSION FOR SMALL AND TOUCH SCREENS
+// adapted from https://www.youtube.com/watch?v=1SxMWhqYQ4k&t=1588s and https://www.youtube.com/watch?v=C7NsIRhoWuE 
+
+let correctScore = 0;
+
+function check() {
+let postylka = $("#input-postylka").val();
+let okno = $("#input-okno").val();
+let kocka = $("#input-kocka").val();
+let zachod = $("#input-zachod").val();
+let vaza = $("#input-vaza").val();
+let lednice = $("#input-lednice").val();
+
+
+if (postylka == "4") {
+  correctScore++;
+  $("#input-postylka").css("background-color", "#caf3e8"); //correct answer
+} else {
+  $("#input-postylka").css("background-color", "#ffded6"); //incorrect answer
+}
+
+if (okno == "2") {
+  correctScore++;
+  $("#input-okno").css("background-color", "#caf3e8"); //correct answer
+} else {
+  $("#input-okno").css("background-color", "#ffded6"); //incorrect answer
+}
+
+if (kocka == "1") {
+  correctScore++;
+  $("#input-kocka").css("background-color", "#caf3e8"); //correct answer
+} else {
+  $("#input-kocka").css("background-color", "#ffded6"); //incorrect answer
+}
+
+if (zachod == "3") {
+  correctScore++;
+  $("#input-zachod").css("background-color", "#caf3e8"); //correct answer
+} else {
+  $("#input-zachod").css("background-color", "#ffded6"); //incorrect answer
+}
+
+if (vaza == "5") {
+  correctScore++;
+  $("#input-vaza").css("background-color", "#caf3e8"); //correct answer
+} else {
+  $("#input-vaza").css("background-color", "#ffded6"); //incorrect answer
+}
+
+if (lednice == "6") {
+  correctScore++;
+  $("#input-lednice").css("background-color", "#caf3e8"); //correct answer
+} else {
+  $("#input-lednice").css("background-color", "#ffded6"); //incorrect answer
+}
+
+$("#results-correct").css("visibility","visible").addClass("after-submit");
+$("#results-correct").html("You got " + correctScore + " correct.");
+
+
+};
+
+// ----VERSION FOR LARGE SCREENS - DRAG AND DROP
+
+// jquery
+
+let correct = 0;
+
+$(function() {
+  
+$( "#postylka, #okno, #kocka, #zachod, #vaza, #lednice" ).draggable();
+
+$( "#drop-postylka" ).droppable({
+  accept: "#postylka",
+  
+  drop: function() {
+    $( this )
+    .addClass("drop-correct");      
+    correct++;
+  }
+});
+
+$( "#drop-okno" ).droppable({
+  accept: "#okno",
+
+  drop: function( event, ui ) {
+    $( this )
+        .addClass("drop-correct");  
+        correct++;
+  }
+});
+
+$( "#drop-kocka" ).droppable({
+  accept: "#kocka",
+
+  drop: function( event, ui ) {
+    $( this )
+        .addClass("drop-correct");  
+        correct++;
+  }
+});
+
+$( "#drop-zachod" ).droppable({
+  accept: "#zachod",
+  
+  drop: function( event, ui ) {
+    $( this )
+        .addClass("drop-correct");    
+        correct++;
+  }
+});
+
+$( "#drop-vaza" ).droppable({
+  accept: "#vaza",
+
+  drop: function( event, ui ) {
+    $( this )
+        .addClass("drop-correct");
+        correct++;
+  }
+});
+
+$( "#drop-lednice" ).droppable({
+  accept: "#lednice",
+
+  drop: function( event, ui ) {
+    $( this )
+        .addClass("drop-correct");
+        correct++;
     }
-    
-},1000)
+  });
 
-function displayTime(second) {
-    const sec = Math.floor(second % 60);
-    timeCountdown.innerHTML = `00:${sec<10?'0':''}${sec}`;
+});
+
+function checkLg() {
+  if (correct > 2){
+    correct = 2;
+  }
+  $(".drop-correct").css("visibility","visible");
+  $("#results-correct").css("visibility","visible").addClass("after-submit");
+  $("#results-correct").html("You got " + correct + " correct.");
 }
 
-function endTime(){
-    timeCountdown.innerHTML = `TIME OUT`;
-}
+/* adapted from https://stackoverflow.com/questions/8752541/jquery-delay-after-click/8752555 */
+
+$("#check-lg").click(
+    function() {
+      setTimeout(
+          function() {
+            if (correct === 2) {
+              $("#congratulations").css("display","block");
+            } else {
+              $("#another-try").css("display","block");
+            }
+          },
+          500);
+  });
+
+  $("#check-sm").click(
+    function() {
+      setTimeout(
+          function() {
+            if (correctScore === 2) {
+              $("#congratulations").css("display","block");
+            } else {
+              $("#another-try").css("display","block");
+            }
+          },
+          500);
+  });
+ 
+
+  function hide() {
+    $("#congratulations").css("display","none");
+    $("#another-try").css("display","none");
+
+  }
+
+  function instructions() {
+    $("#detailed-instructions-lg").css("display","block").addClass("detailed-instructions-lg");
+
+  }
---
+++ assets/js/scriptTimer.js
@@ -0,0 +1,28 @@
+
+/*-------------------------TIMER AND GO TO GAME PAGE-----------------------------------*/
+/* adapted from https://www.youtube.com/watch?v=_a4XCarxwr8&t=333s "How to Create a Countdown Timer with JavaScript" */
+
+const timeCountdown = document.getElementById("timer-font-lg");
+let timeSecond = 40;
+
+displayTime(timeSecond);
+
+const countdown = setInterval (() => {
+    timeSecond--;
+    displayTime(timeSecond);
+    if(timeSecond <= 0 || timeSecond < 1){
+        endTime();
+        clearInterval(countdown);
+        window.location.href = "game.html";
+    }
+    
+},1000)
+
+function displayTime(second) {
+    const sec = Math.floor(second % 60);
+    timeCountdown.innerHTML = `00:${sec<10?'0':''}${sec}`;
+}
+
+function endTime(){
+    timeCountdown.innerHTML = `TIME OUT`;
+}
